3b49c177c5c5cbe63dbd0726bdff41f0b49d34d6
Thijs_Lappy <t.j.kok@st.hanze.nl>
2022-07-29 17:13:42 +0200

    started

---
+++ HelloYou.java
@@ -0,0 +1,9 @@
+// this is a comment!
+public class HelloYou {
+    // first you define a class. within the class you make a method called MAIN
+    // syntax within the {} is part of the class
+    public static void main(String[] args){
+        // syntax here is part of the method
+
+    }
+}
